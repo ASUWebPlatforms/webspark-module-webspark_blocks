b1580f06bf6aeacf7d7663d75b870621ca4f6c1a
Bryan R <aubjr@asu.edu>
2023-03-29 18:15:45 -0700

    WS2-1563 - Integrated ASU Brand local site settings, including opt-out option

--- src/Form/WebsparkBlocksAsuSearchForm.php
+++ src/Form/WebsparkBlocksAsuSearchForm.php
@@ -55,9 +55,19 @@ class WebsparkBlocksAsuSearchForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $url_host = (Html::escape($_SERVER['HTTP_HOST'])) ?? '';
     $keyword = $form_state->getValue('search') ?? ''; // Should be sanitized on search.asu.edu as output to avoid double-sanitizing issues
-    $response = new TrustedRedirectResponse('https://search.asu.edu/?q=' . $keyword . '&url_host=' . $url_host . '&sort=date%3AD%3AL%3Ad1&search-tabs=all');
+    if (\Drupal::moduleHandler()->moduleExists('asu_brand')) {
+      $search_config = \Drupal::config('asu_brand.settings');
+      $search_url = $search_config->get('asu_brand.asu_brand_search_url');
+      // Domain-specific results host
+      $local_search_url = $search_config->get('asu_brand.asu_brand_local_search_url') ?? \Drupal::request()->getHost();
+      // "opt-out" for use by ASU CMS, Integrated Search or with special dispensation
+      $url_host = ($local_search_url === 'opt-out') ? '' : $local_search_url;
+    } else {
+      $search_url = 'https://search.asu.edu/search'; // Default for ASU searches
+      $url_host = \Drupal::request()->getHost() ?? '';
+    }
+    $response = new TrustedRedirectResponse($search_url . '?q=' . $keyword . '&url_host=' . $url_host . '&sort=date%3AD%3AL%3Ad1&search-tabs=all');
     $form_state->setResponse($response);
   }
 }
